9fb95abedf3b0d84d9614bf31a89257652de861b
lreyesp26 <lreyesp@uteq.edu.ec>
2024-01-13 15:36:08 -0500

    Compra falta editar  + Procesamiento app

--- Compra/models.py
+++ Compra/models.py
@@ -2,21 +2,21 @@ from django.db import models
 from Administrador.models import Administrador
 from Proveedores.models import Proveedores
 from Producto.models import *
+from Procesamiento.models import Procesamiento
 
-# Suponiendo que Procesamiento está definido en el mismo archivo models.py
 class Compras(models.Model):
     id_compra = models.AutoField(primary_key=True)
     id_proveedor = models.ForeignKey(Proveedores, models.DO_NOTHING, db_column='id_proveedor')
     id_administrador = models.ForeignKey(Administrador, models.DO_NOTHING, db_column='id_administrador')
-    fechaCompra = models.DateTimeField()
-    id_procesamiento = models.ForeignKey('Procesamiento', models.DO_NOTHING, db_column='id_procesamiento')
-
+    fechacompra = models.DateTimeField()
+    id_procesamiento = models.ForeignKey(Procesamiento, models.DO_NOTHING, db_column='id_procesamiento')
+      
     class Meta:
         managed = False
         db_table = 'compras'
 
 class DetalleCompra(models.Model):
-    id_detalleCompra = models.AutoField(primary_key=True)
+    id_detallecompra = models.AutoField(primary_key=True)
     id_compra = models.ForeignKey(Compras, models.DO_NOTHING, db_column='id_compra')
     id_producto = models.ForeignKey(Producto, models.DO_NOTHING, db_column='id_producto', blank=True, null=True)
     id_componente = models.ForeignKey(Componente, models.DO_NOTHING, db_column='id_componente', blank=True, null=True)
